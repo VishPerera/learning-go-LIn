1cbb6c679ebae1a960dc8b560fa85664e1155c00
VishPerera <34713837+VishPerera@users.noreply.github.com>
2023-03-05 17:25:47 -0700

    User Input from stdin

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,22 @@
+// Getting user input from the Console
+
 package main
 
 import (
+	"bufio" // for Input Output purposes
 	"fmt"
+	"os" // platform-independent interface to operating system for IO
 )
 
 func main() {
 
-	var aString string = "This is Go!"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
+	// variable to store the user input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Text: ") // requst user to input text
 
+	// pasue the application and let user type in
+	// variable input stores whatever user types in
+	// to ignore a variable name it with _
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered: ", input)
 }
