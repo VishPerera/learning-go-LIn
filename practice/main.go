6141c5228beb5b0397f5aae1a7e55ab366425f18
VishPerera <34713837+VishPerera@users.noreply.github.com>
2023-03-06 00:29:27 -0700

    Using Structs in Go

--- practice/main.go
+++ practice/main.go
@@ -1,3 +1,7 @@
+// Grouping realted values using Structs
+// Structs are data structures
+// similar to C's structs
+// Struct is a custom data type
 package main
 
 import (
@@ -5,5 +9,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)                                        //	%+v gives more information for debugging
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight) // accessing values using . operator
+	poodle.Weight = 8                                                  // changing individual values
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+}
+
+// defining a structure
+type Dog struct {
+	Breed  string
+	Weight int
 }
