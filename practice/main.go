8722b151565cc40b0dde499d7dcfddc051a1f3f8
VishPerera <34713837+VishPerera@users.noreply.github.com>
2023-03-05 22:04:17 -0700

    How pointers are used. Basic use is demonstrated

--- practice/main.go
+++ practice/main.go
@@ -1,3 +1,5 @@
+// Reference values with pointers
+
 package main
 
 import (
@@ -5,5 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 45
+	var p = &anInt                  // stores the address of anInt variable in p
+	fmt.Println("Address of p:", p) // Address stored in p
+	fmt.Println("Value of p:", *p)  // Value stored at the address reffered by p
+
+	value1 := 42.6
+	pointer1 := &value1
+	fmt.Println("Value 1", *pointer1)
+
+	// Original variable value can be changed by
+	// manipulating the dereferenced pointer
+	*pointer1 = *pointer1 / 24
+	fmt.Println("Pointer 1", *pointer1)
+	fmt.Println("Value 1", value1)
+
 }
