9f8887a6a1df334e78b6c8a11b4ff889caaca99e
VishPerera <34713837+VishPerera@users.noreply.github.com>
2023-03-06 12:11:21 -0700

    Use of if conditional logic

--- practice/main.go
+++ practice/main.go
@@ -1,3 +1,7 @@
+// Conditional Logic
+// if statement does not need parentheses () for the conditonal statement
+// Also have the option to initialize variables in the codiditional statement
+
 package main
 
 import (
@@ -5,5 +9,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	theAnswer := 45
+	var result string
+
+	// No need of parentheses for the conditional statement
+	if theAnswer < 0 {
+		result = "Less Than Zero"
+	} else if theAnswer == 0 {
+		result = "Equal to Zero"
+	} else {
+		result = "Greater Than Zero"
+	}
+	fmt.Println(result)
+
+	// Variables can be initialized in the conditional statement
+	if x := -36; x < 0 {
+		result = "Less Than Zero"
+	} else if x == 0 {
+		result = "Equal to Zero"
+	} else {
+		result = "Greater Than Zero"
+	}
+	fmt.Println(result)
+
 }
