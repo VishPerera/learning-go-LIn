e402e6030380fbf44ea27c1e010e4d8ff4e0f9c9
VishPerera <34713837+VishPerera@users.noreply.github.com>
2023-03-06 17:21:35 -0700

    Read a text file from the web

--- practice/main.go
+++ practice/main.go
@@ -1,11 +1,32 @@
+// Read a text file from the web
+
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+
+	resp, err := http.Get(url) // download the JSON content into my machine
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response Type %T\n", resp)
+
+	// The Body field is public and can be read
+	defer resp.Body.Close() // close the body after everything is done (in the following lines)
+
+	bytes, err := ioutil.ReadAll(resp.Body) // A byte array is received when read
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Print(content)
 }
